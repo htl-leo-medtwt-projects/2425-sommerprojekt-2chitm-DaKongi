3a7b8f9179e1ba1e7fbf04641248108a4b7a602d
Manuel Kemmer <manuel.kemmer@gmx.net>
2025-06-02 11:38:31 +0200

    some fixes

--- project/script/landingPage.js
+++ project/script/landingPage.js
@@ -12,3 +12,27 @@ window.addEventListener("scroll", function () {
 function changeExampleImage(imageName){
     document.getElementById("switchingImage").style.backgroundImage = `url(img/cubeExample/${imageName}.png)`;
 }
+
+let whatIsMasterCubeContentArray = [
+    {"name":"TUTORIALS","text":"There's nothing you can't find there"},
+    {"name":"TIMER","text":"Know your speed and ignore your limits"},
+    {"name":"COMPETITION","text":"Compete - Win - Repeat It's not that hard"},
+    {"name":"SHOP PORTAL","text":"Yess you need a 10th speedcube (trust me)"}
+];
+
+let currentWhatIsMasterCubeContentArrayIndex = 0;
+
+function nextWhatIsMasterCubeBox(){
+    currentWhatIsMasterCubeContentArrayIndex++;
+    updateWhatIsMasterCubeBox();
+}
+
+function previousWhatIsMasterCubeBox(){
+    currentWhatIsMasterCubeContentArrayIndex--;
+    updateWhatIsMasterCubeBox();
+}
+
+function updateWhatIsMasterCubeBox(){
+    document.getElementById("whatIsMastercubeTitle").innerHTML = whatIsMasterCubeContentArray[currentWhatIsMasterCubeContentArrayIndex].name;
+    document.getElementById("whatIsMastercubeText").innerHTML = whatIsMasterCubeContentArray[currentWhatIsMasterCubeContentArrayIndex].text;
+}
